b3aa1a02901a90844c9c8293486723b3904cd285
Sejun Im <sejun425@gmail.com>
2017-11-06 15:49:54 +0900

    format_suffix_pattern

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from .views import snippet_list, snippet_detail
 
@@ -6,3 +7,5 @@ urlpatterns = [
     url(r'^$', snippet_list, name='snippet_list'),
     url(r'^(?P<pk>\d+)/$', snippet_detail, name='snippet_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)

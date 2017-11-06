3f22d46c31fe9dd52809912a1d0298b37f720afc
Sejun Im <sejun425@gmail.com>
2017-11-06 14:40:40 +0900

    snippet_detail

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 
-from .views import snippet_list
+from .views import snippet_list, snippet_detail
 
 urlpatterns = [
-    url(r'^$', snippet_list, name='snippet_list')
+    url(r'^$', snippet_list, name='snippet_list'),
+    url(r'^(?P<pk>\d+)/$', snippet_detail, name='snippet_detail'),
 ]
